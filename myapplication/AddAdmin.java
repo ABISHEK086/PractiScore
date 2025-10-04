c0e591f75be6b5332a84212ce39a20d9d3e623ae
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-04 09:38:10 +0530

    Update AddAdmin.java

--- myapplication/AddAdmin.java
+++ myapplication/AddAdmin.java
@@ -7,7 +7,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
