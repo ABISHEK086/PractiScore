fc955e396eec705ae38a513827a6f83441cf0f55
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-04 09:32:22 +0530

    Update AddAdmin.java

--- myapplication/AddAdmin.java
+++ myapplication/AddAdmin.java
@@ -28,7 +28,7 @@ public class AddAdmin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_admin);
 
-        // Initialize Firebase Authenticat
+        // Initialize Firebase Authentica
         mAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("users").child("admins");
 
