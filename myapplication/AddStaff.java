7e1b088a451dfae029bf9635d62929ee9b6134de
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-05 13:06:40 +0530

    Update AddStaff.java

--- myapplication/AddStaff.java
+++ myapplication/AddStaff.java
@@ -28,7 +28,7 @@ public class AddStaff extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_staff);
 
-        // Initialize Firebase Authentica
+        // Initialize Firebase Authentic
         mAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("users").child("staff");
 
