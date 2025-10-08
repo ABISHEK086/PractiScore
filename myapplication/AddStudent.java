3f73500760b36d137cd106d70c4a4f8ca46be90e
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-08 09:45:16 +0530

    Update AddStudent.java

--- myapplication/AddStudent.java
+++ myapplication/AddStudent.java
@@ -25,7 +25,7 @@ public class AddStudent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
 
-        // Initiali
+        // Initial
         databaseReference = FirebaseDatabase.getInstance().getReference("users").child("students");
 
         // Initialize UI elements
