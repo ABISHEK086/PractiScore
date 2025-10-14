60ce683dc52de37bee82ec660c8c79d3d9173621
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-14 18:29:37 +0530

    Update LoginStudent.java

--- myapplication/LoginStudent.java
+++ myapplication/LoginStudent.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;  
 import android.util.Log;  
-import android.widget.Button; 
+import android.widget.Button;  
 import android.widget.EditText;
 import android.widget.Toast;    
 import androidx.activity.EdgeToEdge;
