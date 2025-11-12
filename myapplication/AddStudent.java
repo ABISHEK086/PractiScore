9a1d493ea14d5a0b83734d459ce3d10c87d3daa4
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-12 19:59:55 +0530

    Update AddStudent.java

--- myapplication/AddStudent.java
+++ myapplication/AddStudent.java
@@ -1,7 +1,7 @@
 package com.example.myapplication; 
  
 import android.os.Bundle; 
-import android.view.View; 
+import android.view.View;  
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
