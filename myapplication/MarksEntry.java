63650912889ce54f3eab43ed5335c575a9e29612
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-28 16:23:31 +0530

    Update MarksEntry.java

--- myapplication/MarksEntry.java
+++ myapplication/MarksEntry.java
@@ -17,7 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DatabaseReference; 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.io.File;
