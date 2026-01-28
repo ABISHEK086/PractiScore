e942a694e01990c332b9652e1915ade9a855fdb5
ABISHEK ANTONY <abishek8673@gmail.com>
2026-01-28 16:22:28 +0530

    Update MarksEntry.java

--- myapplication/MarksEntry.java
+++ myapplication/MarksEntry.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText; 
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.Nullable;
+import androidx.annotation.Nullable 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
