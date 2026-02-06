05c3b22fb36f27f8210e3d9e2d6d21a7aa6eea4f
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-06 12:02:24 +0530

    Fix formatting in ViewMarks.java

--- myapplication/ViewMarks.java
+++ myapplication/ViewMarks.java
@@ -8,7 +8,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;  
 import android.widget.TextView;  
 import android.widget.Toast;    
- 
+  
 import androidx.annotation.NonNull;  
 import androidx.appcompat.app.AppCompatActivity; 
   
