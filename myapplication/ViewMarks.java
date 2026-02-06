e3ca5d73b7bb6d487a7b46ab640504529edfe206
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-06 12:07:55 +0530

    Update ViewMarks.java

--- myapplication/ViewMarks.java
+++ myapplication/ViewMarks.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;    
 import androidx.appcompat.app.AppCompatActivity;     
    
-import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DataSnapshot; 
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
